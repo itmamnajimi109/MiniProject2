f6f555ef65fe743f80e52f428fca9234d2413ce4
itmamnajimi109 <itmamnajimi109@gmail.com>
2026-04-16 00:22:47 +0800

    change bm to bi

--- login.php
+++ login.php
@@ -22,7 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         header("Location: index.php");
         exit();
     } else {
-        $error = "Username atau Password salah!";
+        $error = "Invalid Username or Password!";
     }
 }
 ?>
