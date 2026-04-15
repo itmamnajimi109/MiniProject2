3996d554bdc54809e18bb5b7ae6c1c1f0bee0872
itmamnajimi109 <itmamnajimi109@gmail.com>
2026-04-15 23:53:09 +0800

    commit change conn

--- conn.php
+++ conn.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-    $host   = 'localhost';
-    $db     = 'pcrs';
-    $user   = 'root';
-    $pass   = '';
+    $host   = 'lsql100.infinityfree.com';
+    $db     = 'if0_41661234_pcrs';
+    $user   = 'if0_41661234';
+    $pass   = 'Watermelon109';
 
     try{
         $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
